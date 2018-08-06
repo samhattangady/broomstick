f4b9640c99ae475cf343dd4bea08b6a8f60bf511
Samarth Hattangady <samhattangady@gmail.com>
2018-08-06 07:38:16 +0530

    Read all values on board.

--- broomstick.py
+++ broomstick.py
@@ -6,13 +6,30 @@ Even mines apparently.
 import pyautogui
 from PIL import Image
 
-unclicked_tile = Image.open('tiles/none.png')
-tiles = pyautogui.locateAllOnScreen(unclicked_tile)
-tiles = list(tiles)
+TILES = {
+          '_': Image.open('tiles/none.png'),
+          0: Image.open('tiles/0.png'),
+          1: Image.open('tiles/1.png'),
+          2: Image.open('tiles/2.png'),
+          3: Image.open('tiles/3.png'),
+          4: Image.open('tiles/4.png'),
+          5: Image.open('tiles/5.png'),
+          6: Image.open('tiles/6.png'),
+          7: Image.open('tiles/7.png'),
+          8: Image.open('tiles/8.png'),
+          'M': Image.open('tiles/mine.png')
+        }
 
-print(len(tiles))
+all_tiles = []
+for tile in TILES:
+    positions = pyautogui.locateAllOnScreen(TILES[tile])
+    for p in positions:
+        all_tiles.append({'value': tile, 'position': p})
+all_tiles = sorted(all_tiles, key=lambda x: (x['position'][1], x['position'][0]))
 
-tiles = sorted(tiles, key=lambda x: (x[1], x[0]))
-x,y = tiles[0][0]+5, tiles[0][1]+5
+width = len(set([x['position'][0] for x in all_tiles]))
+for i, tile in enumerate(all_tiles):
+    print(tile['value'], end=' ')
+    if (i+1)%width == 0:
+        print()
 
-pyautogui.click(x,y)
